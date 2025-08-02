1501323e9c5fbaff1ec94925f82c58e265805053
seraphimm777 <145017278+seraphimm777@users.noreply.github.com>
2025-08-02 19:48:59 +0530

    age_calculator.py

---
+++ age_calculator.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+birth_year = input("Enter your birth year: ")
+birth_year = int(birth_year)
+current_year = 2025
+age = current_year - birth_year
+print("Hi", name + "!", "You are", age, "years old.")
